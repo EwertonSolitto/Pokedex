e4ef2d394d19f6a3ae0a9f192f8d25a890d50ce9
Ewerton <ewerton2004@yahoo.com.br>
2024-02-21 21:20:51 -0300

    feat(pokemon card): finish pokemon card

--- src/app/components/PokemonCard.tsx
+++ src/app/components/PokemonCard.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import Pokemon from "../models/Pokemon";
+import bgTypes from "../classes/bgTypes";
+import pokeballTypes from "../classes/pokeballTypes";
 
 
 export default function PokemonCard(props: Pokemon) {
   const { name, id, height, weight, type } = props
-  const allColorTypes = ["bg-bug", "bg-dark", "bg-dragon", "bg-electric", "bg-fairy", "bg-flying", "bg-fighting", "bg-fire", "bg-ghost", "bg-grass", "bg-ground", "bg-ice", "bg-normal", "bg-poison", "bg-psychic", "bg-rock", "bg-steel", "bg-water"
-]
+  const tailwindLoader = [bgTypes, pokeballTypes]
+
+  const firstType = type[0]
 
   const pokemonNumber = new String((`${'0'.repeat(4 - id.toString().length)}${id}#`))
 
   return (
     <li className="w-80  h-[7.5rem] bg-gray-100 border-red-800 border-solid border-8 rounded-full flex items-center">
-      <div className="w-24 h-24 rounded-full flex items-center justify-center bg-gray-400 ml-1 bg-[url('/pokeballs/grass-pokeball.svg')] bg-cover rotate-[-40deg]">
+      <div className={`w-24 h-24 rounded-full flex items-center justify-center bg-gray-400 ml-1 bg-[url('/pokeballs/${firstType.type}-pokeball.svg')] bg-cover rotate-[-40deg]`}>
         <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} alt={`name sprite`} width={80} height={80} className="w-20 h-20 rotate-[40deg]"/>
       </div>
       <div className="text-gray-400 w-[10.5rem] h-full ml-1 py-1 flex flex-col justify-between">
@@ -41,7 +44,7 @@ export default function PokemonCard(props: Pokemon) {
           }
         </div>
       </div>
-      <Image src={'/pokeballs/grass-pokeball.svg'} alt="green pokeball" width={64} height={64} className="absolute ml-48 mb-5 opacity-50" />
+      <Image src={`/pokeballs/${firstType.type}-pokeball.svg`} alt="green pokeball" width={64} height={64} className="absolute ml-48 mb-5 opacity-20" />
     </li>
   )
 }
