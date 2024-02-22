3fbec9bfdf10b8690a85b1d6a8ddeb2b71710b24
Ewerton <ewerton2004@yahoo.com.br>
2024-02-21 21:19:47 -0300

    feat(classes): create files to load some tailwind classes

---
+++ src/app/classes/bgTypes.ts
@@ -0,0 +1,22 @@
+const bgTypes = [
+  "bg-bug", 
+  "bg-dark", 
+  "bg-dragon", 
+  "bg-electric", 
+  "bg-fairy", 
+  "bg-flying", 
+  "bg-fighting", 
+  "bg-fire", 
+  "bg-ghost", 
+  "bg-grass", 
+  "bg-ground", 
+  "bg-ice", 
+  "bg-normal", 
+  "bg-poison", 
+  "bg-psychic", 
+  "bg-rock", 
+  "bg-steel", 
+  "bg-water"
+]
+
+export default bgTypes;
---
+++ src/app/classes/pokeballTypes.ts
@@ -0,0 +1,22 @@
+const pokeballTypes = [
+  "bg-[url('/pokeballs/bug-pokeball.svg')]", 
+  "bg-[url('/pokeballs/dark-pokeball.svg')]",
+  "bg-[url('/pokeballs/dragon-pokeball.svg')]",
+  "bg-[url('/pokeballs/electric-pokeball.svg')]",
+  "bg-[url('/pokeballs/fairy-pokeball.svg')]",
+  "bg-[url('/pokeballs/fighting-pokeball.svg')]",
+  "bg-[url('/pokeballs/fire-pokeball.svg')]",
+  "bg-[url('/pokeballs/flying-pokeball.svg')]",
+  "bg-[url('/pokeballs/ghost-pokeball.svg')]",
+  "bg-[url('/pokeballs/grass-pokeball.svg')]",
+  "bg-[url('/pokeballs/ground-pokeball.svg')]",
+  "bg-[url('/pokeballs/ice-pokeball.svg')]",
+  "bg-[url('/pokeballs/normal-pokeball.svg')]",
+  "bg-[url('/pokeballs/poison-pokeball.svg')]",
+  "bg-[url('/pokeballs/psychic-pokeball.svg')]",
+  "bg-[url('/pokeballs/rock-pokeball.svg')]",
+  "bg-[url('/pokeballs/steel-pokeball.svg')]",
+  "bg-[url('/pokeballs/water-pokeball.svg')]"
+]
+
+export default pokeballTypes
